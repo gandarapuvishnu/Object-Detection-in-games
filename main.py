9f65b6331d5e09cf59c5ea201b58893d503014c9
Gandarapu Vishnu <52231690+gandarapuvishnu@users.noreply.github.com>
2021-01-12 14:30:13 +0530

    removed extras

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     # List of the strings that is used to add correct label for each box.
     PATH_TO_LABELS = os.path.join('object_detection', 'data', 'mscoco_label_map.pbtxt')
 
-    Detect(PATH_TO_CKPT, PATH_TO_LABELS )
+    Detect(PATH_TO_CKPT, PATH_TO_LABELS)
